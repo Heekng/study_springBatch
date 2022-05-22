d1dcb03bc46d1ddd8fecaed6988a57d2c21a4289
heekng <rhkd1769@gmail.com>
2022-05-22 16:59:22 +0900

    Transition - 배치상태유형

--- src/main/java/com/heekng/springbatch/JobConfiguration.java
+++ src/main/java/com/heekng/springbatch/JobConfiguration.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,37 +17,24 @@ public class JobConfiguration {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job batchJob() {
-        // --job.name=batchJob
-        return jobBuilderFactory.get("batchJob")
-                .start(flowA())
-                .next(step3())
-                .next(flowB())
-                .next(step6())
-                .end()
+    public Job batchJob1() {
+        // simpleJob
+        // --job.name=batchJob1
+        return jobBuilderFactory.get("batchJob1")
+                .start(step1())
+                .next(step2())
                 .build();
     }
-
     @Bean
-    public Flow flowA() {
-        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowA");
-
-        flowBuilder.start(step1())
-                .next(step2())
-                .end();
-
-        return flowBuilder.build();
-    }
-
-    @Bean
-    public Flow flowB() {
-        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowB");
-
-        flowBuilder.start(step4())
-                .next(step5())
-                .end();
-
-        return flowBuilder.build();
+    public Job batchJob2() {
+        // flowJob
+        // --job.name=batchJob2
+        return jobBuilderFactory.get("batchJob2")
+                .start(step1())
+                .on("FAILED")
+                .to(step2())
+                .end()
+                .build();
     }
 
     @Bean
@@ -57,6 +42,7 @@ public class JobConfiguration {
         return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("step1 has executed");
+                    contribution.setExitStatus(ExitStatus.FAILED);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
@@ -67,47 +53,6 @@ public class JobConfiguration {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("step2 has executed");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step3 has executed");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step4 has executed");
-                    throw new RuntimeException("step1 was failed");
-//                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step5() {
-        return stepBuilderFactory.get("step5")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step5 has executed");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step6() {
-        return stepBuilderFactory.get("step6")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step6 has executed");
                     return RepeatStatus.FINISHED;
                 })
                 .build();

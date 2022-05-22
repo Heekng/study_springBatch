4fcc362e2848a6b21e7821325a62e6eb73c3ce3c
heekng <rhkd1769@gmail.com>
2022-05-22 20:52:24 +0900

    사용자정의 ExitStatus

--- src/main/java/com/heekng/springbatch/JobConfiguration.java
+++ src/main/java/com/heekng/springbatch/JobConfiguration.java
@@ -1,6 +1,7 @@
 package com.heekng.springbatch;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class JobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
@@ -23,15 +25,8 @@ public class JobConfiguration {
                 .start(step1())
                     .on("FAILED")
                     .to(step2())
-                    .on("FAILED")
+                    .on("PASS")
                     .stop()
-                .from(step1())
-                    .on("*")
-                    .to(step3())
-                    .next(step4())
-                .from(step2())
-                    .on("*")
-                    .to(step5())
                     .end()
                 .build();
     }
@@ -40,8 +35,8 @@ public class JobConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step1 has executed");
-                    contribution.setExitStatus(ExitStatus.FAILED);
+                    log.warn("step1 has executed");
+                    contribution.getStepExecution().setExitStatus(ExitStatus.FAILED);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
@@ -51,39 +46,10 @@ public class JobConfiguration {
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step2 has executed");
+                    log.warn("step2 has executed");
                     return RepeatStatus.FINISHED;
                 })
-                .build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step3 has executed");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step4 has executed");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step5() {
-        return stepBuilderFactory.get("step5")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step5 has executed");
-                    return RepeatStatus.FINISHED;
-                })
+                .listener(new PassCheckingListener())
                 .build();
     }
 

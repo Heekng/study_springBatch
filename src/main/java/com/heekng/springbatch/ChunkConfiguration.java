36478c13072526c143168a8c9a4f775211f68195
heekng <rhkd1769@gmail.com>
2022-06-06 18:16:54 +0900

    staxEventItemReader

--- src/main/java/com/heekng/springbatch/ChunkConfiguration.java
+++ src/main/java/com/heekng/springbatch/ChunkConfiguration.java
@@ -5,20 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.file.transform.Range;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.xstream.XStreamMarshaller;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Configuration
@@ -33,50 +31,50 @@ public class ChunkConfiguration {
     public Job job() {
         // --job.name=batchJob
         return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
                 .start(step1())
-                .next(step2())
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<String, String>chunk(3)
-                .reader(itemReader())
-                .writer(new ItemWriter() {
-                    @Override
-                    public void write(List items) throws Exception {
-                        items.forEach(item -> log.warn("item = {}", item));
-                    }
-                })
+                .<Customer, Customer>chunk(3)
+                .reader(customItemReader())
+                .writer(customItemWriter())
                 .build();
     }
 
     @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet((contribution, chunkContext) -> {
-                    log.warn("step2 has executed");
-                    return RepeatStatus.FINISHED;
-                })
+    public ItemReader<? extends Customer> customItemReader() {
+        return new StaxEventItemReaderBuilder<Customer>()
+                .name("statXml")
+                .resource(new ClassPathResource("customer.xml"))
+                .addFragmentRootElements("customer")
+                .unmarshaller(itemUnmarshaller())
                 .build();
     }
 
     @Bean
-    public FlatFileItemReader itemReader() {
-        return new FlatFileItemReaderBuilder<Customer>()
-                .name("flatFile")
-                .resource(new FileSystemResource("/Users/heekng/heekngCoding/inflean_study/spring-batch/src/main/resources/customer.txt"))
-                .fieldSetMapper(new BeanWrapperFieldSetMapper<>())
-                .targetType(Customer.class)
-                .linesToSkip(1)
-                .fixedLength()
-                .strict(false) // parsing 예외를 잡지 않는다.
-                .addColumns(new Range(1, 5))
-                .addColumns(new Range(6, 9))
-                .addColumns(new Range(10, 11))
-                .names("name", "year", "age")
-                .build();
+    public Unmarshaller itemUnmarshaller() {
+
+        Map<String, Class<?>> aliases = new HashMap<>();
+        aliases.put("customer", Customer.class);
+        aliases.put("id", Long.class);
+        aliases.put("name", String.class);
+        aliases.put("age", Integer.class);
+
+        XStreamMarshaller xStreamMarshaller = new XStreamMarshaller();
+        xStreamMarshaller.setAliases(aliases);
+
+        return xStreamMarshaller;
+    }
+
+    @Bean
+    public ItemWriter<Customer> customItemWriter() {
+        return items -> {
+            items.forEach(item -> log.warn(item.toString()));
+        };
     }
 
 }
--- src/main/java/com/heekng/springbatch/Customer.java
+++ src/main/java/com/heekng/springbatch/Customer.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class Customer {
 
-    private String name;
-    private int age;
-    private String year;
+    private final long id;
+    private final String name;
+    private final int age;
 
 }

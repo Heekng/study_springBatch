f3d43442329dd6367b7b1c0534e2f949d5cd0a5a
heekng <rhkd1769@gmail.com>
2022-06-08 16:16:02 +0900

    JdbcBatchItemWriter

--- src/main/java/com/heekng/springbatch/ChunkConfiguration.java
+++ src/main/java/com/heekng/springbatch/ChunkConfiguration.java
@@ -6,13 +6,15 @@ import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.adapter.ItemReaderAdapter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,7 +26,7 @@ public class ChunkConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-    private final EntityManagerFactory entityManagerFactory;
+    private final DataSource dataSource;
 
     @Bean
     public Job job() throws Exception {
@@ -38,30 +40,46 @@ public class ChunkConfiguration {
     @Bean
     public Step step1() throws Exception {
         return stepBuilderFactory.get("step1")
-                .<String, String>chunk(10)
+                .<Customer, Customer>chunk(10)
                 .reader(customItemReader())
                 .writer(customItemWriter())
                 .build();
     }
 
-    private ItemReader<String> customItemReader() {
-        ItemReaderAdapter<String> reader = new ItemReaderAdapter<>();
-        reader.setTargetObject(customService());
-        reader.setTargetMethod("customRead");
+    @Bean
+    public ItemWriter<? super Customer> customItemWriter() {
+        return new JdbcBatchItemWriterBuilder<Customer>()
+                .dataSource(dataSource)
+                .sql("insert into customer2 values (:id, :firstName, :lastName, :birthdate)")
+                .beanMapped()
+                .build();
+    }
+
+    @Bean
+    public JdbcPagingItemReader<Customer> customItemReader() {
+        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
+
+        reader.setDataSource(this.dataSource);
+        reader.setFetchSize(10);
+        reader.setRowMapper(new CustomerRowMapper());
+
+        MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
+        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+        queryProvider.setFromClause("from customer");
+        queryProvider.setWhereClause("where firstName like :firstname");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+
+        sortKeys.put("id", Order.ASCENDING);
+        queryProvider.setSortKeys(sortKeys);
+        reader.setQueryProvider(queryProvider);
+
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("firstname", "A%");
+
+        reader.setParameterValues(parameters);
+
         return reader;
-    }
-
-    @Bean
-    public Object customService() {
-        return new CustomService();
-    }
-
-
-    @Bean
-    public ItemWriter<String> customItemWriter() {
-        return items -> {
-            items.forEach(item -> log.warn(item));
-        };
     }
 
 }

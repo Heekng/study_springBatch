77b7124577cd716a699732e08cee857b60d53123
heekng <rhkd1769@gmail.com>
2022-06-08 14:32:54 +0900

    JdbcCursorItemReader

--- src/main/java/com/heekng/springbatch/ChunkConfiguration.java
+++ src/main/java/com/heekng/springbatch/ChunkConfiguration.java
@@ -1,10 +1,5 @@
 package com.heekng.springbatch;
 
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.security.NoTypePermission;
-import com.thoughtworks.xstream.security.NullPermission;
-import com.thoughtworks.xstream.security.PrimitiveTypePermission;
-import com.thoughtworks.xstream.security.TypePermission;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.*;
@@ -13,17 +8,11 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.json.JacksonJsonObjectReader;
-import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
-import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.oxm.Unmarshaller;
-import org.springframework.oxm.xstream.XStreamMarshaller;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -33,6 +22,8 @@ public class ChunkConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private int chunkSize = 10;
+    private final DataSource dataSource;
 
     @Bean
     public Job job() {
@@ -46,7 +37,7 @@ public class ChunkConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<Customer, Customer>chunk(3)
+                .<Customer, Customer>chunk(chunkSize)
                 .reader(customItemReader())
                 .writer(customItemWriter())
                 .build();
@@ -54,10 +45,14 @@ public class ChunkConfiguration {
 
     @Bean
     public ItemReader<? extends Customer> customItemReader() {
-        return new JsonItemReaderBuilder<Customer>()
-                .name("jsonReader")
-                .resource(new ClassPathResource("customer.json"))
-                .jsonObjectReader(new JacksonJsonObjectReader<>(Customer.class))
+
+        return new JdbcCursorItemReaderBuilder<Customer>()
+                .name("jdbcCursorItemReader")
+                .fetchSize(chunkSize)
+                .sql("select id, firstName, lastName, birthdate from customer where firstName like ? order by lastName, firstName")
+                .beanRowMapper(Customer.class)
+                .queryArguments("A%")
+                .dataSource(dataSource)
                 .build();
     }
 
